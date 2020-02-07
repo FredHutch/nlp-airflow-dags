54d148a8e5df85d071ac107447ca8eb0074229d0
Dora Dong <39133175+ddong63@users.noreply.github.com>
2020-02-07 15:24:01 -0800

    flask endpoint fix (#61)
    the missing '/'

--- active/utilities/flask_blob_nlp.py
+++ active/utilities/flask_blob_nlp.py
@@ -21,7 +21,7 @@ def call_flask_blob_nlp_endpoint(blobid, hdcpupdatedate, note, endpoint):
     call flaskblobnlp to process the de-id notes
     """
     try:
-        resp = common.FLASK_BLOB_NLP_API_HOOK.run("/{}".format(endpoint),
+        resp = common.FLASK_BLOB_NLP_API_HOOK.run("/{}/".format(endpoint),
                             data=json.dumps({"extract_text": note}),
                             headers={"Content-Type": "application/json"})
         result = json.loads(resp.content)
