71d386938e2965c46d3e28ebe312cb7601026b16
Ritche Long <rlong@fredhutch.org>
2020-02-19 16:38:58 -0800

    Initial pass at testing enqueue/requeue ops

--- active/test-queue-dags.py
+++ active/test-queue-dags.py
@@ -6,11 +6,10 @@ import base64
 from airflow.hooks import HttpHook, MsSqlHook, PostgresHook
 from airflow.contrib.hooks.ssh_hook import SSHHook
 from airflow.operators.python_operator import PythonOperator
-from utilities.job_states import JOB_RUNNING, JOB_COMPLETE, JOB_FAILURE, REVIEW_BYPASSED_ANNOTATION_TYPE, BRAT_REVIEWED_ANNOTATION_TYPE
+import utilities.common as common
 from airflow.models import DAG
-import operators.trashman as trashman
 
-DAG_NAME ='queue-dags'
+DAG_NAME ='test-queue-dags'
 
 args = {
     'owner': 'whiteau',
@@ -24,20 +23,28 @@ dag = DAG(dag_id=DAG_NAME,
 	      default_args=args,
 	      dagrun_timeout=timedelta(seconds=30))
 
+def enqueue(upstream_task, **kwargs):
+  upstream_task = kwargs['upstream_task']
+  (run_id, date_stamp) = kwargs['ti'].xcom_pull(task_ids=upstream_task)
+  run_id = run_id + '88888' # so i can see this sifting through the queue
+  exec_stmt = ("EXEC dbo.sp_requeue_note_id 'dbo.clinical_notes_process_queue', %s, %s")
+  common.ANNOTATIONS_DB.run(exec_stmt, parameters=(run_id, date_stamp), autocommit=True)
+
+def dequeue(**kwargs):
+  exec_stmt = ("EXEC dbo.sp_dequeue_note_id 'dbo.clinical_notes_process_queue'")
+  results = common.ANNOTATIONS_DB.get_first(exec_stmt, autocommit=True)
+  print(results)
+  return (results[0], results[1])
+
 enqueue = PythonOperator(task_id='enqueue',
 	                             provide_context=True,
-	                             python_callable=enqueue_job,
+	                             python_callable=enqueue,
 	                             dag=dag)
+
 dequeue = PythonOperator(task_id='dequeue',
                                provide_context=True,
-                               python_callable=dequeue_job,
+                               python_callable=dequeue,
                                dag=dag)
 
 dequeue >> enqueue
 
-def enqueue_job():
-
-  pass
-
-def dequeue_job():
-  pass
