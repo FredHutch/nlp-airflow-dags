47c94cd95a3db0b995346c641d87cbcd7a5e3880
Silgard <esilgard@fredhutch.org>
2020-04-06 12:23:47 -0700

    type error on annotation grab fail

--- active/af3-resynthesize-phi.py
+++ active/af3-resynthesize-phi.py
@@ -125,7 +125,7 @@ def populate_blobid_in_job_table(**kwargs):
             message = ("An OperationalError occured while trying to fetch potential annotations from the source data server"
                    " for select statement {}".format(src_select_stmt))
             print(message)
-            common_functions.log_error_and_failure_for_resynth_note_job(run_id, 'Unknown', 'Unknown', message,
+            common_functions.log_error_and_failure_for_resynth_note_job(run_id, 0, 0, message,
                                                                   "Get potential annotations")
         
 
