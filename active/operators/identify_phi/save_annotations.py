e8aa68753cbc8f1bb4023fa8d7037f22b3522bf1
Scott White <whiteau@fhcrc.org>
2020-04-16 14:49:02 -0700

    additional empty record checks in save_annotations module

--- active/operators/identify_phi/save_annotations.py
+++ active/operators/identify_phi/save_annotations.py
@@ -2,9 +2,13 @@ import utilities.common_functions as common_functions
 
 def save_deid_annotations(annotation_records):
     for blobid, record in annotation_records.items():
+        if not record:
+            continue
         common_functions.save_deid_annotation(blobid, record['hdcpupdatedate'], str(record['annotated_note']))
 
 
 def save_unreviewed_annotations(annotation_records):
     for blobid, record in annotation_records.items():
+        if not record:
+            continue
         common_functions.save_unreviewed_annotation(blobid, record['hdcpupdatedate'], str(record['annotated_note']))
