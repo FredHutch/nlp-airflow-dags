ae6319c324734c4023659565daba27e1d25e0a32
Emily <esilgard@users.noreply.github.com>
2020-04-21 15:34:38 -0700

    update date diff function (#124)
    Co-authored-by: Silgard <esilgard@fredhutch.org>

--- active/operators/trashman/check_brat_completeness.py
+++ active/operators/trashman/check_brat_completeness.py
@@ -26,25 +26,26 @@ def check_brat_completeness(upstream_task, **kwargs):
     return (run_id, complete_brat_files)
 
 def _get_complete_brat_notes_from_db(check_date):
-    src_select_stmt = ("SELECT b.{brat_id}, b.brat_last_modified_date, b.directory_location, b.hdcorcablobid, b.hdcpupdatedate "
+    src_select_stmt = ("SELECT b.{brat_id}, b.brat_last_modified_date, b.directory_location, "
+                        "b.hdcorcablobid, b.hdcpupdatedate "
                         "FROM {brat_table} as b "
                         "LEFT JOIN {job_table} as j "
-                        "  ON b.{brat_id} = j.{brat_id} "
+                        "ON b.{brat_id} = j.{brat_id} "
                         "WHERE b.job_status = '{complete_status}' "
-                        "AND b.brat_last_modified_date >=  dateadd(day,{threshold},{current_date}) "
+                        "AND b.brat_last_modified_date <= DATE('{current_date}') - INTERVAL '{threshold} day'"
                         "AND j.{brat_id} is NULL ".format(brat_table=common_variables.AF4_SOURCE_BRAT_TABLE,
                                                         brat_id = common_variables.AF4_SOURCE_BRAT_ID,
                                                         job_table=common_variables.AF4_COMPLETE_BRAT_TABLE,
                                                         complete_status=common_variables.BRAT_COMPLETE,
-                                                        threshold=common_variables.COMPLETE_STALE_THRESHOLD,
-                                                        current_date=check_date))
+                                                        current_date=check_date
+                                                        threshold=common_variables.COMPLETE_STALE_THRESHOLD))
 
     completed_notes = (common_hooks.AIRFLOW_NLP_DB.get_records(src_select_stmt) or [])
     dict_notes = [{'brat_id': n[0],
-                       'last_update_date': n[1],
-                       'directory_location':n[2],
-                       'hdcorcablobid': n[3],
-                       'hdcpupdatedate': n[4]} for n in completed_notes]
+                   'last_update_date': n[1],
+                   'directory_location':n[2],
+                   'hdcorcablobid': n[3],
+                   'hdcpupdatedate': n[4]} for n in completed_notes]
 
     return dict_notes
 
