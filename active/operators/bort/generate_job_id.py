19b2ac3690a550eba1a6bbee4b5d4facb5e9109a
Ritche Long <rlong@fredhutch.org>
2020-04-22 08:33:53 -0700

    Add proper revision/changes

--- active/operators/bort/generate_job_id.py
+++ active/operators/bort/generate_job_id.py
@@ -10,7 +10,7 @@ def generate_job_id(**kwargs):
     """
     last_run_id = _get_last_bort_run_id()
     #TODO implement _get_last_bort_update_date()
-    last_bort_update_date = _get_last_resynth_update_date()
+    last_bort_update_date = _get_last_bort_update_date()
 
     if last_run_id is None:
         new_run_id = 1
@@ -26,16 +26,18 @@ def generate_job_id(**kwargs):
     job_start_date = datetime.now().strftime(common_variables.DT_FORMAT)[:-3]
     bortdates = []
 
-    blob_job_queue = _get_blobs_since_date(date=last, job_state=common_variables.JOB_COMPLETE)
+    blob_job_queue = _get_blobs_since_date(date=last_bort_update_date, job_state=NLP)
 
-    #check the queue for any new jobs that need to be scheduled
     if blob_job_queue is None:
-      exit()
+        print("No new records found since last update date: {}".format(last_bort_update_date))
+        exit()
     else:
-      for row in blob_job_queue:
-        bortdates.append(row[1])
-    return new_run_id, bortdates
+        for row in blob_job_queue:
+            bortdates.append(row[1])
+            _insert_bort_scheduled(new_run_id, row[1], job_start_date)
 
+        print("{} new update batches found since last update date: {}".format(len(bort_dates)
+    return new_run_id, bortdates
 
 def _insert_bort_scheduled(run_id, update_date, job_start_date, **kwargs):
     """
@@ -44,19 +46,13 @@ def _insert_bort_scheduled(run_id, update_date, job_start_date, **kwargs):
     :param update_date:  bortdate from the af_bort_runs_details table where bortstatus == complete
     """
     tgt_insert_stmt = "INSERT INTO {table}" \
-                      "({run_id}, job_status, job_start, bortdate) " \
+                      "({run_id}, job_status, job_start, bort_date) " \
                       "VALUES (%s, %s, %s, %s)".format(table=common_variables.AF6_RUNS,
                                                        run_id=common_variables.AF6_RUNS_ID)
     common_hooks.AIRFLOW_NLP_DB.run(tgt_insert_stmt,
                               parameters=(run_id, common_variables.JOB_RUNNING, job_start_date, update_date))
 
     return
-
-def _get_blobs_since_date(date, job_state, **kwargs):
-  """
-  :param date:
-  :param job_state:
-  """
 
 def _get_last_bort_run_id(**kwargs):
     tgt_select_stmt = "SELECT max({run_id}) FROM {table}".format(table=common_variables.AF6_RUNS,
@@ -65,9 +61,21 @@ def _get_last_bort_run_id(**kwargs):
 
     return last_run_id[0]
 
+def _get_blobs_since_date(date, job_state, **kwargs):
+    """
+    :param date: last resynth task complete date
+    :param state: resynth job complete
+    """
+    tgt_update_stmt = "SELECT distinct hdcorcablobid, ner_date " \
+                      "FROM {table} " \
+                      "WHERE ner_date >= %s " \
+                      "AND ner_status = %s ".format(table=common_variables.AF5_RUNS_DETAILS)
+    return common_hooks.AIRFLOW_NLP_DB.get_records(tgt_update_stmt, parameters=(date, job_state))
+
+
 
 def _get_last_bort_update_date(**kwargs):
-    tgt_select_stmt = "SELECT max(bortdate) " \
+    tgt_select_stmt = "SELECT max(bort_date) " \
                       "FROM {table} " \
                       "WHERE job_status = %s".format(table=common_variables.AF6_RUNS)
     last_bort_update_date = (common_hooks.AIRFLOW_NLP_DB.get_first(tgt_select_stmt,
